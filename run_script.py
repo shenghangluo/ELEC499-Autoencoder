58cb4136e8fb25d540fb86568a5c444ef11d1f31
Ali Saberali <saberali@ece.ubc.ca>
2019-10-16 00:20:00 -0700

    Remove all code in run_script.py now contained in other modeules

--- run_script.py
+++ run_script.py
@@ -1,48 +1,16 @@
-import tensorflow as tf
-import dataset
-import encoder as enc
-import decoder as dec
-import loss
+from trainer import Trainer
+import config
 
-X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
-y = tf.placeholder(tf.int32, shape=(None), name="y")
 
-encoder = enc.Encoder(inputs=X, n_layers=2, n_neurons=25, activation=tf.nn.relu, latent_size=10)
-z = encoder.get_latent_representation()
+def run_experiment():
+    params = config.create_params()
+    trainer = Trainer(**params)
+    trainer.train_model()
 
-decoder = dec.Decoder(inputs=z, n_layers=2, n_neurons=25, activation=tf.nn.relu, output_size=28*28)
-Xhat = decoder.get_outputs()
 
-loss = loss.Loss(loss_type='mse')
-mse = loss.get_loss(target=X, prediction=Xhat)
+if __name__ == "__main__":
+    run_experiment()
 
-learning_rate = 0.01
-
-with tf.name_scope("train"):
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    training_op = optimizer.minimize(loss)
-
-def shuffle_batch(X, y, batch_size):
-    rnd_idx = np.random.permutation(len(X))
-    n_batches = len(X) // batch_size
-    for batch_idx in np.array_split(rnd_idx, n_batches):
-        X_batch, y_batch = X[batch_idx], y[batch_idx]
-        yield X_batch, y_batch
-
-n_epochs = 20
-n_batches = 50
-
-init = tf.global_variables_initializer()
-with tf.Session() as sess:
-    init.run()
-    for epoch in range(n_epochs):
-        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
-            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
-        acc_batch = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
-        acc_valid = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
-        print(epoch, "Batch accuracy:", acc_batch, "Validation accuracy:", acc_valid)
-
-    save_path = saver.save(sess, "./my_model_final.ckpt")
 
 
 
