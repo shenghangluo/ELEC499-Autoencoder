1cbf558648950c21b4ae6af4c53fbe68d5e3dc50
Lutz Lampe <lampe@Lampe-3.local>
2019-10-17 10:34:00 -0700

    Delete unused variables in Model class in model.py

--- model.py
+++ model.py
@@ -7,16 +7,12 @@ import loss
 
 class Model(object):
     def __init__(self, dataset_params, encoder_params, decoder_params):
-        self._dataset_params = dataset_params
-        self._encoder_params = encoder_params
-        self._decoder_params = decoder_params
+        self.X = tf.placeholder(tf.float32, shape=(None, dataset_params['n_features']), name="X")
 
-        self.X = tf.placeholder(tf.float32, shape=(None, self._dataset_params['n_features']), name="X")
-
-        self._encoder = enc.Encoder(inputs=self.X, **self._encoder_params)
+        self._encoder = enc.Encoder(inputs=self.X, **encoder_params)
         z = self._encoder.get_latent_representation()
 
-        self._decoder = dec.Decoder(inputs=z, **self._decoder_params)
+        self._decoder = dec.Decoder(inputs=z, **decoder_params)
         self._Xhat = self._decoder.get_outputs()
 
     def get_reconstruction(self):
