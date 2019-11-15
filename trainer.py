5cae53a5d5d274c7ef0b7bb9100e51ffde12bb5f
Lutz Lampe <lampe@dhcp-128-189-69-60.ubcsecure.wireless.ubc.ca>
2019-11-14 18:58:09 -0800

    Add tensorboard summaries

--- trainer.py
+++ trainer.py
@@ -11,15 +11,26 @@ class Trainer(object):
         self._dataset = DatasetMNIST(val_size=10000)
         self._model = Model(dataset_params=dataset_params, encoder_params=encoder_params, decoder_params=decoder_params)
         self._optimizer = tf.train.GradientDescentOptimizer(learning_rate=optimizer_params['lr'])
+        self._writer = tf.summary.FileWriter('./summary')
 
     def train_model(self):
         data = self._dataset.load_data()
         loss = tf.reduce_mean(tf.square(self._model.X-self._model.get_reconstruction()))
-        training_op = self._optimizer.minimize(loss)
+        tf.summary.scalar('Loss', loss)
+        training_op = self._optimizer.grad(loss)
+
+        summary_op = tf.summary.merge_all()
         init = tf.global_variables_initializer()
         with tf.Session() as sess:
             init.run()
             for epoch in range(self._n_epochs):
+                train_loss = 0
                 for X_batch, y_batch in self._dataset.shuffle_batch(data['X_train'], data['y_train'], self._batch_size):
-                    sess.run(training_op, feed_dict={self._model.X: X_batch})
-                print(epoch, "Training Loss:", loss.eval(feed_dict={self._model.X: data['X_valid']}))
+                    _, loss_batch = sess.run([training_op, loss], feed_dict={self._model.X: X_batch})
+                    train_loss += loss_batch
+
+                summary = sess.run(summary_op, feed_dict={self._model.X: data['X_valid']})
+                self._writer.add_summary(summary=summary, global_step=epoch)
+                # TODO(ali): Clean this code for the LOVE PF GOD!!!
+                print(epoch, "Training Loss:", train_loss/250, "Validation Loss",
+                      loss.eval(feed_dict={self._model.X: data['X_valid']}))
